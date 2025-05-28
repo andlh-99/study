11a0993b7acdb86cbe593d19643beff05514750a
andlh-99 <abdellahferrahi99@gmail.com>
2025-05-28 21:51:35 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: './', // اجعل المسارات نسبية لجعل التطبيق يعمل من أي مكان
+  build: {
+    outDir: 'dist', // تأكد أن Vite يبني إلى مجلد dist
+    emptyOutDir: true, // لحذف الملفات القديمة قبل البناء
+  },
 });
